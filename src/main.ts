caa1a789a24c6d0d026a3c74a24f34e7c735fc61
Сергей Гутовский <sergey.gutovskiy@infourok.biz>
2023-07-21 15:28:26 +0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import App from './App.vue';
 import Home from './pages/Home/Home.vue';
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
