049eb4e5f630c4dcf1aa5e832e070fbdd586cac7
Сергей Гутовский <sergey.gutovskiy@infourok.biz>
2023-07-21 15:01:53 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/banda-test'
+  base: 'https://sergeygutovskiy.github.io/banda-test/'
 })
